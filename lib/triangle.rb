56dc3b594e1eb8eec3e3799a25f4ca0527c0460b
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-10-31 00:09:16 +0300

    DONE

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_reader = :l1, :l2,:l3
+
+  def initialize(l1,l2,l3)
+    @l1 = l1 
+    @l2 = l2 
+    @l3 = l3
+  end
+
+  def kind
+    raise TriangleError unless valid_triangle?
+        if @l1.eql?(@l2) && @l2.eql?(@l3)
+          :equilateral
+        elsif @l1.eql?(@l2) || @l2.eql?(@l3) || @l1.eql?(@l3)
+          :isosceles
+        else 
+          :scalene 
+      end
+  end 
+  
+  def valid_triangle?
+    (@l1 && @l2 && @l3) > 0 && @l1 + @l2 > @l3 && @l1 + @l3 > @l2 && @l2 + @l3 > @l1
+  end 
+
+  class TriangleError < StandardError
+    def warning 
+      "Input a valid triangle"
+    end 
+  end 
+
 end
